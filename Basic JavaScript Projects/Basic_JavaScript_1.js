047bd289090081d5d252c6527575f572f95d52ad
HeidiZEdwards <56694950+HeidiZEdwards@users.noreply.github.com>
2019-11-09 08:04:54 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+var Dad = "Bob";
+var Dad = Dad.fontcolor ("blue");
+document.write (Dad);
+var Mom = "Jane";
+var Mom = Mom.fontcolor ("red");
+document.write (Mom);
+var Daughter = "Penny";
+var Daughter = Daughter.fontcolor ("green");
+document.write (Daughter);
+
+var B = "This is the Jones" + "family"
