e29cac51021df77466b734c872b22b9110fa0835
HeidiZEdwards <56694950+HeidiZEdwards@users.noreply.github.com>
2019-11-10 09:19:39 -0800

    Add files via upload

--- Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -7,3 +7,6 @@ document.write (Mom);
 var Daughter = "Penny";
 var Daughter = Daughter.fontcolor ("green");
 document.write (Daughter);
+
+var B = " This is the Jones" + " family!";
+document.write (B);
